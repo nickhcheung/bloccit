d25989a2676979b097b9a3766d6382b428bc6dcf
Nick Cheung <nickhcheung@hotmail.com>
2017-05-19 13:42:07 -0700

    Add sign in view and sessions controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe UsersController, type: :controller do
 
     it "sets user name properly" do
       post :create, user: new_user_attributes
-      expect(assigns(:user).name).to eq new_user_attributes[:name]
+      expect(assigns(:user).name).to eq new_user_attributes[:name].capitalize
     end
 
     it "sets user email properly" do
@@ -47,6 +47,11 @@ RSpec.describe UsersController, type: :controller do
     it "sets user password properly" do
       post :create, user: new_user_attributes
       expect(assigns(:user).password).to eq new_user_attributes[:password]
+    end
+
+    it "logs the user in after sign up" do
+      post :create, user: new_user_attributes
+      expect(session[:user_id]).to eq assigns(:user).id
     end
   end
 end
