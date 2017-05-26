953e17553d204bc5524970563adf6827a1627d31
Nick Cheung <nickhcheung@hotmail.com>
2017-05-25 20:21:03 -0700

    Add association between posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require "random_data"
 
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -17,6 +26,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
@@ -31,7 +41,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: "nickhcheung@hotmail.com",
+  password: "123456"
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
