2d339f441f92a2fce6d3dbec6d396dadc4f36018
Nick Cheung <nickhcheung@hotmail.com>
2017-05-29 14:50:50 -0700

    Add authorization restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,14 @@ class User < ActiveRecord::Base
       self.name = cap_name.join(" ")
     end
   }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
+
   has_secure_password
+
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -41,10 +41,17 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: "nickhcheung@hotmail.com",
-  password: "123456"
+admin = User.create!(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "helloworld",
+  role: "admin"
+)
+
+member = User.create!(
+  name: "Member User",
+  email: "member@example.com",
+  password: "helloworld"
 )
 
 puts "Seed finished"
