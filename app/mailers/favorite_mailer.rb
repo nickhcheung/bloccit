1553df1e774855369837853fb15cfcf9a9d0b453
Nick Cheung <nickhcheung@hotmail.com>
2017-06-05 14:43:21 -0700

    Assignment 31 Favorites: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on#{post.title}", cc: "nickhcheung@hotmail.com")
   end
+
+  def new_post(user, post)
+    headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, subject: "Your post (#{post.title}) has been favorited.")
+  end
 end
